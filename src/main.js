8b800c7350a64b92f177ad6671917b0501fa5028
RomanDovgii <romandovgiiwork@gmail.com>
2020-04-02 18:29:49 +0300

    update 0.1.2
    Changed files:
    - main.js
    
    Changes:
    - Removed 3 arrays which contained cards for movies from main movie 
    list, top rated movie list, most commented movie list.
    - Reworked createMovieCardTemplate function which starts at line 87. 
    There was added the movie card for "The Dance of Life" movie as a 
    template for movie cards.
    - Added cardsRender function which replaced 3 for loops which added 
    movie cards. 
    
    -ru-
    Обновление 0.1.2
    
    Измененные файлы:
    - main.js
    
    Изменения: 
    - Убрано три массива, которые содержали карточки для фильмов из 
    основного списка фильмов, списка фильмов с лучшими оценками, списка 
    фильмов с наибольшим количеством комментариев.
    - Переработана функция createMovieCardTemplate, которая начинается на 
    строке 87. Там была добавлена карточка фильма "The Dance of Life" как 
    шаблон для карточек фильмов. 
    - Добавлена функция cardsRender, которая заменила три цикла for, которые 
    добавляли карточки фильмов.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 "use strict";
 
 const moviesToRender = 5;
-const commentedMoviesToRender = 2;
-const ratedMoviesToRender = 2;
+const extraMoviesToRender = 2;
 
 const Positions = {
   BEFORE_BEGIN: `beforebegin`,
@@ -10,148 +9,6 @@ const Positions = {
   BEFORE_END: `beforeend`,
   AFTER_END: `afterend`
 };
-
-const movies = [
-  `<h3 class="film-card__title">The Dance of Life</h3>
-  <p class="film-card__rating">8.3</p>
-  <p class="film-card__info">
-    <span class="film-card__year">1929</span>
-    <span class="film-card__duration">1h 55m</span>
-    <span class="film-card__genre">Musical</span>
-  </p>
-  <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-  <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-  <a class="film-card__comments">5 comments</a>
-  <form class="film-card__controls">
-    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-    <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-  </form>`,
-  `<h3 class="film-card__title">Sagebrush Trail</h3>
-  <p class="film-card__rating">3.2</p>
-  <p class="film-card__info">
-    <span class="film-card__year">1933</span>
-    <span class="film-card__duration">54m</span>
-    <span class="film-card__genre">Western</span>
-  </p>
-  <img src="./images/posters/sagebrush-trail.jpg" alt="" class="film-card__poster">
-  <p class="film-card__description">Sentenced for a murder he did not commit, John Brant escapes from prison determined to find the real killer. By chance Brant's narrow escap…</p>
-  <a class="film-card__comments">89 comments</a>
-  <form class="film-card__controls">
-    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist film-card__controls-item--active">Add to watchlist</button>
-    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-    <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-  </form>`,
-  `<h3 class="film-card__title">The Man with the Golden Arm</h3>
-  <p class="film-card__rating">9.0</p>
-  <p class="film-card__info">
-    <span class="film-card__year">1955</span>
-    <span class="film-card__duration">1h 59m</span>
-    <span class="film-card__genre">Drama</span>
-  </p>
-  <img src="./images/posters/the-man-with-the-golden-arm.jpg" alt="" class="film-card__poster">
-  <p class="film-card__description">Frankie Machine (Frank Sinatra) is released from the federal Narcotic Farm in Lexington, Kentucky with a set of drums and a new outlook on…</p>
-  <a class="film-card__comments">18 comments</a>
-  <form class="film-card__controls">
-    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched film-card__controls-item--active">Mark as watched</button>
-    <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-  </form>`,
-  `<h3 class="film-card__title">Santa Claus Conquers the Martians</h3>
-  <p class="film-card__rating">2.3</p>
-  <p class="film-card__info">
-    <span class="film-card__year">1964</span>
-    <span class="film-card__duration">1h 21m</span>
-    <span class="film-card__genre">Comedy</span>
-  </p>
-  <img src="./images/posters/santa-claus-conquers-the-martians.jpg" alt="" class="film-card__poster">
-  <p class="film-card__description">The Martians Momar ("Mom Martian") and Kimar ("King Martian") are worried that their children Girmar ("Girl Martian") and Bomar ("Boy Marti…</p>
-  <a class="film-card__comments">465 comments</a>
-  <form class="film-card__controls">
-    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-    <button class="film-card__controls-item button film-card__controls-item--favorite film-card__controls-item--active">Mark as favorite</button>
-  </form>`,
-  `<h3 class="film-card__title">Popeye the Sailor Meets Sindbad the Sailor</h3>
-  <p class="film-card__rating">6.3</p>
-  <p class="film-card__info">
-    <span class="film-card__year">1936</span>
-    <span class="film-card__duration">16m</span>
-    <span class="film-card__genre">Cartoon</span>
-  </p>
-  <img src="./images/posters/popeye-meets-sinbad.png" alt="" class="film-card__poster">
-  <p class="film-card__description">In this short, Sindbad the Sailor (presumably Bluto playing a "role") proclaims himself, in song, to be the greatest sailor, adventurer and…</p>
-  <a class="film-card__comments">0 comments</a>
-  <form class="film-card__controls">
-    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist film-card__controls-item--active">Add to watchlist</button>
-    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched film-card__controls-item--active">Mark as watched</button>
-    <button class="film-card__controls-item button film-card__controls-item--favorite film-card__controls-item--active">Mark as favorite</button>
-  </form>`
-];
-const moviesRated = [
-  `<h3 class="film-card__title">The Man with the Golden Arm</h3>
-  <p class="film-card__rating">9.0</p>
-  <p class="film-card__info">
-    <span class="film-card__year">1955</span>
-    <span class="film-card__duration">1h 59m</span>
-    <span class="film-card__genre">Drama</span>
-  </p>
-  <img src="./images/posters/the-man-with-the-golden-arm.jpg" alt="" class="film-card__poster">
-  <p class="film-card__description">Frankie Machine (Frank Sinatra) is released from the federal Narcotic Farm in Lexington, Kentucky with a set of drums and a new outlook on…</p>
-  <a class="film-card__comments">18 comments</a>
-  <form class="film-card__controls">
-    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched  film-card__controls-item--active">Mark as watched</button>
-    <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-  </form>`,
-  `<h3 class="film-card__title">The Great Flamarion</h3>
-  <p class="film-card__rating">8.9</p>
-  <p class="film-card__info">
-    <span class="film-card__year">1945</span>
-    <span class="film-card__duration">1h 18m</span>
-    <span class="film-card__genre">Mystery</span>
-  </p>
-  <img src="./images/posters/the-great-flamarion.jpg" alt="" class="film-card__poster">
-  <p class="film-card__description">The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Grea…</p>
-  <a class="film-card__comments">12 comments</a>
-  <form class="film-card__controls">
-    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-    <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-  </form>`
-];
-const moviesCommented = [
-  `<h3 class="film-card__title">Santa Claus Conquers the Martians</h3>
-  <p class="film-card__rating">2.3</p>
-  <p class="film-card__info">
-    <span class="film-card__year">1964</span>
-    <span class="film-card__duration">1h 21m</span>
-    <span class="film-card__genre">Comedy</span>
-  </p>
-  <img src="./images/posters/santa-claus-conquers-the-martians.jpg" alt="" class="film-card__poster">
-  <p class="film-card__description">The Martians Momar ("Mom Martian") and Kimar ("King Martian") are worried that their children Girmar ("Girl Martian") and Bomar ("Boy Marti…</p>
-  <a class="film-card__comments">465 comments</a>
-  <form class="film-card__controls">
-    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-    <button class="film-card__controls-item button film-card__controls-item--favorite film-card__controls-item--active">Mark as favorite</button>
-  </form>`,
-  `<h3 class="film-card__title">Made for Each Other</h3>
-  <p class="film-card__rating">5.8</p>
-  <p class="film-card__info">
-    <span class="film-card__year">1939</span>
-    <span class="film-card__duration">1h 32m</span>
-    <span class="film-card__genre">Comedy</span>
-  </p>
-  <img src="./images/posters/made-for-each-other.png" alt="" class="film-card__poster">
-  <p class="film-card__description">John Mason (James Stewart) is a young, somewhat timid attorney in New York City. He has been doing his job well, and he has a chance of bei…</p>
-  <a class="film-card__comments">56 comments</a>
-  <form class="film-card__controls">
-    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-    <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-  </form>`
-];
 
 const createUserRank = () => {
   return (
@@ -230,7 +87,22 @@ const createMostCommentedContainer = () => {
 const createMovieCard = () => {
   return (
     `<article class="film-card">
-    </article>`
+        <h3 class="film-card__title">The Dance of Life</h3>
+        <p class="film-card__rating">8.3</p>
+        <p class="film-card__info">
+          <span class="film-card__year">1929</span>
+          <span class="film-card__duration">1h 55m</span>
+          <span class="film-card__genre">Musical</span>
+        </p>
+        <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
+        <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
+        <a class="film-card__comments">5 comments</a>
+        <form class="film-card__controls">
+          <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+          <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+          <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+        </form>
+      </article>`
   );
 };
 
@@ -426,6 +298,12 @@ const render = (container, template, position) => {
   container.insertAdjacentHTML(position, template);
 };
 
+const cardsRender = (container, counter) => {
+  for (let i = 0; i < counter; i++) {
+    render(container, createMovieCard(), Positions.BEFORE_END);
+  }
+};
+
 const body = document.body;
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
@@ -446,50 +324,17 @@ render(films, createTopRatedContainer(), Positions.BEFORE_END);
 render(films, createMostCommentedContainer(), Positions.BEFORE_END);
 
 const filmsList = films.querySelector(`.films-list`);
-
-render(filmsList, createShowMoreButton(), Positions.BEFORE_END);
-
-const filmsContainer = filmsList.querySelector(`.films-list__container`);
-
-for (let i = 0; i < moviesToRender; i++) {
-  render(filmsContainer, createMovieCard(), Positions.BEFORE_END);
-}
-
-const filmsCards = filmsContainer.querySelectorAll(`.film-card`);
-
-let counter = 0;
-filmsCards.forEach((container) => {
-  render(container, movies[counter], Positions.BEFORE_END);
-  counter++;
-});
-
 const topRated = films.querySelector(`.films-list--rated`);
 const topRatedContainer = topRated.querySelector(`.films-list__container`);
 const mostCommented = films.querySelector(`.films-list--commented`);
 const mostCommentedContainer = mostCommented.querySelector(`.films-list__container`);
 
-for (let i = 0; i < ratedMoviesToRender; i++) {
-  render(topRatedContainer, createMovieCard(), Positions.BEFORE_END);
-}
+render(filmsList, createShowMoreButton(), Positions.BEFORE_END);
 
-const topRatedCards = topRatedContainer.querySelectorAll(`.film-card`);
+const filmsContainer = filmsList.querySelector(`.films-list__container`);
 
-counter = 0;
-topRatedCards.forEach((container) => {
-  render(container, moviesRated[counter], Positions.BEFORE_END);
-  counter++;
-});
-
-for (let i = 0; i < commentedMoviesToRender; i++) {
-  render(mostCommentedContainer, createMovieCard(), Positions.BEFORE_END);
-}
-
-const mostCommentedCards = mostCommentedContainer.querySelectorAll(`.film-card`);
-
-counter = 0;
-mostCommentedCards.forEach((container) => {
-  render(container, moviesCommented[counter], Positions.BEFORE_END);
-  counter++;
-});
+cardsRender(filmsContainer, moviesToRender);
+cardsRender(topRatedContainer, extraMoviesToRender);
+cardsRender(mostCommentedContainer, extraMoviesToRender);
 
 render(body, createInfoPopup(), Positions.BEFORE_END);
