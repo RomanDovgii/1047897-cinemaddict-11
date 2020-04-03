570ca2367212443536ca70149eb33855b6aac3c9
RomanDovgii <romandovgiiwork@gmail.com>
2020-04-03 23:38:34 +0300

    Added packages, All HTML element creators were moved into components

---
+++ src/components/topRatedContainer.js
@@ -0,0 +1,10 @@
+export const createTopRatedContainer = () => {
+  return (
+    `<section class="films-list--extra films-list--rated">
+      <h2 class="films-list__title">Top rated</h2>
+
+      <div class="films-list__container">
+      </div>
+    </section>`
+  );
+};
